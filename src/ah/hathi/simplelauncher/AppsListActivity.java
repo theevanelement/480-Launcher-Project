e50336ff4e9cde335e8fbacdaaaf330ad0d5c01d
theevanelement <theevanelement@gmail.com>
2016-01-19 15:42:18 -0500

    Launcher

---
+++ src/ah/hathi/simplelauncher/AppsListActivity.java
@@ -0,0 +1,16 @@
+package ah.hathi.simplelauncher;
+ 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+ 
+public class AppsListActivity extends Activity {
+     
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {    
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps_list);
+    }
+         
+}
